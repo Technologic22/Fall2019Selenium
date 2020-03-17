cc308ec526eed93b368b3065dcccb0ac221ce3ae
Technologic22 <biomuhammet@gmail.com>
2020-03-17 00:00:01 -0400

    Commit 03/16 Title equals test

---
+++ src/test/java/com/automation/tests/day2/BasicNavigation.java
@@ -0,0 +1,42 @@
+package com.automation.tests.day2;
+
+import io.github.bonigarcia.wdm.WebDriverManager;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.remote.RemoteWebDriver;
+import org.openqa.selenium.safari.SafariDriver;
+
+public class BasicNavigation {
+    public static void main(String[] args) throws Exception {
+
+        WebDriverManager.chromedriver().setup();
+        //to start Se script we need:
+       //a- setup webdriver and b-create webdriver object
+
+               WebDriver driver = new ChromeDriver();
+               // in SE everything starts from webdriver interface
+
+
+        //polymorphism starts here
+        //RemoteWebDriver is super class of browser drivers
+
+        driver.get("http://google.com"); //to open webpage
+
+        Thread.sleep(4000); //for demo purpose only!!!
+
+        //U can also see it as tab name: It returns title
+        String title = driver.getTitle();// <title> Some Title </title> text
+        System.out.println(title);
+
+        String expectedTitle = "Google";
+
+        if (expectedTitle.equals(title)){
+            System.out.println("Test Passed");
+        }else {
+            System.out.println("Fail");
+        }
+
+        driver.close(); // Browser doesnt close itself
+
+    }
+}
