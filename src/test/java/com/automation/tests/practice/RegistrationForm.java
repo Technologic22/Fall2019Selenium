05fd7237913ba645081905adda9a5649dc5437e4
Technologic22 <biomuhammet@gmail.com>
2020-04-02 01:02:19 -0400

    Registration Form Practice

--- src/test/java/com/automation/tests/practice/RegistrationForm.java
+++ src/test/java/com/automation/tests/practice/RegistrationForm.java
@@ -4,9 +4,12 @@ import com.automation.utilities.BrowserUtils;
 import io.github.bonigarcia.wdm.WebDriverManager;
 import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
 import org.openqa.selenium.chrome.ChromeDriver;
 import org.openqa.selenium.support.ui.Select;
 import static org.testng.Assert.*;
+
+import org.testng.Assert;
 import org.testng.annotations.AfterMethod;
 import org.testng.annotations.BeforeMethod;
 import org.testng.annotations.Test;
@@ -69,6 +72,24 @@ public class RegistrationForm {
         assertEquals(actual, expected);
 
        }
+
+        @Test(description = "verify userName cant accept character less than 2 more than 64")
+        public void test2(){
+            driver.findElement(firstNameBy).sendKeys("s");
+            BrowserUtils.wait(2);
+
+        WebElement warningMessage=driver.findElement(By.xpath("//small[text()='first name must be more than 2 and less than 64 characters long']"));
+            assertTrue(warningMessage.isDisplayed());
+        }
+
+        @Test (description = "verify username rejects just numerical characters")
+        public void verifyUsernameIsAlphabetical(){
+        driver.findElement(firstNameBy).sendKeys("123456789");
+        BrowserUtils.wait(2);
+        WebElement message=driver.findElement(By.xpath("//small[text()='first name can only consist of alphabetical letters']"));
+        assertTrue(message.isDisplayed());
+
+        }
 
 
     @BeforeMethod
